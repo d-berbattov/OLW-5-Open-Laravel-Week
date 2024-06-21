15f06c3fd8f3fe485d34ef9a290e8eab6972b5be
d-berbattov <berbattov.tech@gmail.com>
2024-06-21 19:12:34 -0300

    ✨ Added front logic that return the products

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class FrontController extends Controller
+{
+  public function buildMenu() // load menu
+  {
+    $brands = Brand::all();
+    $categories = Category::with('products')->get();
+
+    return response(['brands' => $brands, 'categories' => $categories]);
+  }
+
+  public function home() {
+
+    [$brands, $categories, $products] = Cache::tags(['product_related'])->rememberForever('home-cache', function () {
+      $brands = Brand::where('is_featured', 1)
+        ->select('id', 'name')
+        ->get();
+      $categories = Category::where('is_featured', 1)
+        ->select('id', 'name')
+        ->get();
+      $products = Product::with(['skus:id,name,price,product_id', 'skus.images:id,url,sku_id'])
+        ->where('is_featured', 1)
+        ->select('id', 'name', 'slug')
+        ->get();
+
+      return [$brands, $categories, $products];
+    });
+
+
+    return response([
+      'brands' => $brands,
+      'categories' => $categories,
+      'products' => $products
+    ]);
+  }
+
+  public function productAssessories() {         //Load filter content
+
+    $brands = Brand::all();
+    $categories = Category::with('products')->get();
+
+    return response(['brands' => $brands, 'categories' => $categories]);
+  }
+
+  public function products(Request $request) {  //Product list
+
+    $key = "product-request-{$request->page}-{$request->category_id}-{$request->brand_id}{$request->value_type}-{$request->price}";
+    $products = Cache::tags(['product_related'])->rememberForever($key, function() use ($request) {
+      $products = Product::with('skus.images');
+
+      if ($request->filled('category_id')) {
+        $products->where('category_id', $request->get('category_id'));
+      }
+
+      if ($request->filled('brand_id')) {
+        $products->where('brand_id', $request->get('brand_id'));
+      }
+
+      if ($request->filled('value_type') && $request->filled('price')) {
+        $products->whereHas('skus', function ($query) use ($request) {
+          $query->where('price', $request->value_type, $request->get('price'));
+        });
+      }
+
+      $products = $products->paginate(12); //Pagination
+
+      return $products;
+    });
+
+
+    return response()->json($products);
+  }
+
+  public function product(Product $product) { // Return products
+    $product = $product->load('skus.images');
+
+    return response()->json($product);
+  }
+
+  public function static()
+  {
+    return "ok";
+  }
+}
--- app/Services/ProductServices.php
+++ app/Services/ProductServices.php
@@ -2,7 +2,11 @@
 
 namespace App\Services;
 
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
 use App\Http\Requests\ProductStoreRequest;
+use App\Http\Requests\ProductUpdateRequest;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -19,7 +23,6 @@ class ProductServices
 
   public function store(ProductStoreRequest $request) {
     $product = DB::transaction(function () use ($request) {
-      
 
       $product_data = $request->except("sku");
       $product_data['slug'] = Str::slug($product_data['name']);
@@ -40,20 +43,59 @@ class ProductServices
       }
 
       return $product->load('skus.images');
-
     });
 
     return $product;
   }
 
-  public function update(ProductUpdateRequest $request,  Product $products) {
-    //Update product
-    $products->update($request->validated());
-    return $products;
+  public function update(ProductUpdateRequest $request, Product $product) {
+    $product = DB::transaction(function () use ($request, $product) {
+
+      $product_data = $request->except("sku");
+
+      $product_data['slug'] = Str::slug($product_data['name']);
+
+      $product->update($product_data);
+
+      foreach ($request->sku as $key => $sku) {
+
+        $sku = $product->skus()->updateOrCreate(['id' => $sku['id'] ?? 0], $sku);
+
+        if (isset($request->sku[$key]['images'])) {
+          foreach ($request->sku[$key]['images'] as $index => $image) {
+
+            $path = $image["url"]->store('products', 's3');
+
+            $sku->images()->create([
+              'url' => $path,
+              'featured' => $index == 0
+            ]);
+          }
+        }
+      }
+
+      return $product->load('skus.images');
+    });
+
+    return $product;
   }
 
-  public function destroy(Product $products) {
-    //Delete product
-    $products->delete();
+  public function destroy(Product $product)
+  {
+    DB::transaction(function () use ($product) {
+      $product = $product->load('skus.images');
+
+      foreach ($product->skus as $sku) {
+        foreach($sku->images as $image){
+          $image->delete();
+        }
+
+        $sku->delete();
+      }
+
+      $product->delete();
+
+    });
   }
+
 }
